4004b0bced86a6c46dc0214d7b99dd6c0aad7c6c
Sakar Aryal <techiesakar@gmail.com>
2021-05-08 19:14:04 +0545

    Responsive Created

---
+++ assets/js/header.js
@@ -0,0 +1,30 @@
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+function openMenu() {
+    document.getElementById("mobileMenu").style.right = "0";
+    document.getElementById("menuBtn").style.visibility="hidden";
+
+}
+
+function closeMenu() {
+    document.getElementById("mobileMenu").style.right = "-100%";
+    document.getElementById("menuBtn").style.visibility="visible";
+}
+var x = 0;
+
+function openSubMenu() {
+
+    if (x == 0) {
+        document.getElementById("mobileSubMenu").style.display = "block";
+        x = 1;
+    } else {
+        document.getElementById("mobileSubMenu").style.display = "none";
+        x = 0
+    }
+}
